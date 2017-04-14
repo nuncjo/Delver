226f226ee4fe7d2d4446dd4cbf897b902e135c40
Nuncjo <zoreander@gmail.com>
2017-04-14 23:24:31 +0200

    added exceptions

---
+++ delver/exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+
+class GeneralError(Exception):
+    """General error"""
+
+
+class SessionError(GeneralError):
+    """Raised on errors related to session usage."""
+
+
+class HistoryError(GeneralError):
+    """Raised when functionality needs history=True."""
+
+
+class FlowError(GeneralError):
+    """Raised on errors related to flow usage."""
