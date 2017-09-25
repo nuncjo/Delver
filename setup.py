b571ee8a81b70b4aac0fee0fb9eed9da3b0f5d09
Nuncjo <rafal.sobczynski@gmail.com>
2017-09-25 23:16:35 +0200

    release prepare

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    # TODO(nuncjo): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='delver',
+    version='0.1.0',
+    description="Modern user friendly web automation and scraping library.",
+    long_description=readme + '\n\n' + history,
+    author="Nuncjo",
+    author_email='zoreander@gmail.com',
+    url='https://github.com/nuncjo/delver',
+    packages=find_packages(include=['delver']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='delver, web automation, spider, scraper, mechanize, scrapy, robobrowser',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
